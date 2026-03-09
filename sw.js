ac373d9bc69c77bda36d2d065342691d35d622b6
gen8-riv <gen8.riv@gmail.com>
2026-03-10 00:07:20 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+var CACHE_NAME = "kusuri-navi-v1";
+var URLS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.json"
+];
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(name) {
+          return name !== CACHE_NAME;
+        }).map(function(name) {
+          return caches.delete(name);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    fetch(event.request).then(function(response) {
+      var responseClone = response.clone();
+      caches.open(CACHE_NAME).then(function(cache) {
+        cache.put(event.request, responseClone);
+      });
+      return response;
+    }).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
